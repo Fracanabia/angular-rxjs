1c1d600b95e993b362ff7348e04de97c4191a9b5
Wesley Fracanábia dos Santos <wesleyf.santos92@hotmail.com>
2023-03-02 14:28:11 -0300

    feat: ToArray

--- src/app/components/operations-transformation/to-array/to-array-routing.module.ts
+++ src/app/components/operations-transformation/to-array/to-array-routing.module.ts
@@ -6,6 +6,6 @@ const routes: Routes = [{ path: '', component: ToArrayComponent }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class ToArrayRoutingModule { }
+export class ToArrayRoutingModule {}
