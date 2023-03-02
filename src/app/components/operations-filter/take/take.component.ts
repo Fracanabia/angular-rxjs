2d52f40f488a9bbdf2a490e93cc1f1a4c76c60f3
Wesley Fracanábia dos Santos <wesleyf.santos92@hotmail.com>
2023-03-02 14:59:48 -0300

    feat: Take

--- src/app/components/operations-filter/take/take.component.ts
+++ src/app/components/operations-filter/take/take.component.ts
@@ -1,8 +1,49 @@
-import { Component } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { from, interval, map, Observable, Subject, take } from 'rxjs';
 
 @Component({
   selector: 'app-take',
   templateUrl: './take.component.html',
   styleUrls: ['./take.component.scss'],
 })
-export class TakeComponent {}
+export class TakeComponent implements OnInit, OnDestroy {
+  private _destroyed$ = new Subject();
+
+  ngOnInit(): void {
+    this.initOperatorTake();
+  }
+
+  initOperatorTake() {
+    const arrayObject$: Observable<{ id: number; name: string; age: number }> =
+      from([
+        { id: 1, name: 'Wesley', age: 31 },
+        { id: 2, name: 'Ana Paula', age: 29 },
+        { id: 3, name: 'Amanda', age: 24 },
+      ]);
+
+    const names = arrayObject$.pipe(
+      map(data => data.name),
+      take(1)
+    );
+
+    names.subscribe({
+      next: res => {
+        console.log(`res: `, res);
+      },
+    });
+
+    const interval$ = interval(1000);
+    const first5Values = interval$.pipe(take(5));
+
+    first5Values.subscribe({
+      next: res => {
+        console.log(`first5Values: `, res);
+      },
+    });
+  }
+
+  ngOnDestroy(): void {
+    this._destroyed$.next(null);
+    this._destroyed$.unsubscribe();
+  }
+}
